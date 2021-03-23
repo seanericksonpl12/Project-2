bd56f7029f825c18e8b03eb39e12b588a684accf
Werhan <79816680+Werhan@users.noreply.github.com>
2021-03-23 08:19:04 +0800

    Add files via upload

--- Backend.java
+++ Backend.java
@@ -11,12 +11,27 @@ import java.util.HashMap;
 //TA: Hang
 //Lecturer: Florian
 //Notes to Grader: <optional extra notes>
+/**
+ * This is the Backend class that implements BackendInterface
+ * It receives data from DataReader and provide method to the Frontend 
+ * In this class we put Pokemon into a RBT and make use of the 
+ * redblacktree
+ * 
+ * @author Erhan Wang
+ * @version 1.0
+ *
+ */
 public class Backend implements BackendInterface{
   private RedBlackTree<Pokemon> nameRBT;
   private List<Pokemon> PokemonList;
   private List<String> names;
   private HashMap<String,Pokemon> nameHash;
-  
+  /**
+   * Constructor method to initialize a Backend class
+   * 
+   * @throws DataFormatException
+   * @throws IOException
+   */
   public Backend() throws DataFormatException, IOException {
     names = new ArrayList<String>();
     nameHash = new HashMap<String,Pokemon>();
@@ -25,13 +40,20 @@ public class Backend implements BackendInterface{
     nameRBT = new RedBlackTree<Pokemon>();
   }
   
-  
+  /**
+   * return a List that contains Pokemon
+   * @return Pokemon List
+   */
   public List<Pokemon> getPokemon(){
     return PokemonList;
   }
   
   
-
+  /**
+   * When given a String name, search that name and put the pokemon into a RBT and 
+   * a HashTable
+   * @param Name
+   */
   @Override
   public void addName(String Name) {
     if(!names.contains(Name)) {
@@ -47,7 +69,13 @@ public class Backend implements BackendInterface{
     
     return;
   }
-  
+  /**
+   * Check if the provided pokemon is in the List
+   * and then return the pokemon.
+   * 
+   * @param pokemon
+   * @return the searched pokemon
+   */
   public Pokemon checkNames(String pokemon) {
     for (int i = 0; i < PokemonList.size(); i++) {
       if (PokemonList.get(i).getName().equals(pokemon)) {
@@ -57,7 +85,13 @@ public class Backend implements BackendInterface{
     return null;
   }
 
-
+  /**
+   * Check if the provided pokemon is in the Hashtable
+   * and then return the pokemon.
+   * 
+   * @param PokName
+   * @return the searched Pokemon
+   */
   @Override
   public Pokemon getNames(String PokName) {
     
@@ -69,12 +103,19 @@ public class Backend implements BackendInterface{
     }
     
   }
-
+  /**
+   * Get the size of Pokemon List
+   * @return the size of the Pokemon List
+   */
   @Override
   public int getNumberOfPokemons() {
     return PokemonList.size();
   }
-
+  /**
+   * get all the name in the Pokemon List
+   * 
+   * @return all the names in the Pokemon List
+   */
   @Override
   public String getAllNames() {
     List<String> nms = new ArrayList<String>();
@@ -86,7 +127,10 @@ public class Backend implements BackendInterface{
     }
     return nms.toString();
   }
-  
+  /**
+   * Get all the names in the Pokedex
+   * @returnthe name of pokemon in the Pokedex
+   */
   public String pokedexNames() {
     return names.toString();
   }
